30f9e649573ed1d2c4b913f63818aa38638beafb
jose codespace <josemesamarin@gmail.com>
2024-03-01 20:22:21 +0100

    proyecto, fin clase 01/03/24

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+// scss
 import "./scss/styles.scss";
+
+// typescript
+import "./ts/api.js";
+import { getMovieListData } from "./ts/api.js";
+import { movieListType } from "./ts/api.js";
+
+getMovieListData(movieListType.popular, 3)
+  .then((movies) => {
+    console.log(movies);
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
